2689f9de0e64303865eaa7d6852a7c81483a5ddc
Mauricio Cartagena <mc973220@gmail.com>
2021-12-31 13:17:57 -0400

    Agregando mutaticion editar estudiante

--- backend/src/resolvers/estudiante.ts
+++ backend/src/resolvers/estudiante.ts
@@ -12,6 +12,8 @@ import { Persona } from "../entities/Persona";
 import { Estudiante } from "../entities/Estudiante";
 import { EstudianteInput } from "./EstudianteInput";
 import { validateRegisterEstudiante } from "../utils/validateRegisterEstudiante";
+import { EstudianteInputEditar } from "./EstudianteInputEditar";
+import { validateEditarEstudiante } from "../utils/validateEditarEstudiante";
 
 @ObjectType()
 class FieldErrorEstudiante {
@@ -119,50 +121,80 @@ export class EstudianteResolver {
     }
   }
 
-  @Mutation(() => [Persona], { nullable: true })
+  @Mutation(() => EstudianteResponse)
   async actualizarEstudiante(
-    @Arg("id_persona", () => Int) id_persona: number,
-    @Arg("nombre", () => String) nombre: string,
-    @Arg("primer_apellido", () => String)
-    primer_apellido: string,
-    @Arg("segundo_apellido", () => String, { nullable: true })
-    segundo_apellido: string,
-    @Arg("dni", () => String) dni: string,
-    @Arg("numero", () => String) numero: string,
-    @Arg("email", () => String) email: string
-  ): Promise<Persona[] | null> {
+    @Arg("input") input: EstudianteInputEditar
+  ): Promise<EstudianteResponse> {
     const conn = getConnection();
+
+    const errors = validateEditarEstudiante(input);
+
+    // return if exist errors
+    if (errors) {
+      return { errors };
+    }
 
     const estudiante = await conn
       .createQueryBuilder(Persona, "persona")
-      .innerJoin(Estudiante, "estudiante", "persona.id_persona = " + id_persona)
+      .innerJoin(
+        Estudiante,
+        "estudiante",
+        "persona.id_persona = " + input.id_persona
+      )
       .getMany();
 
     if (!estudiante) {
-      return null;
+      return {
+        errors: [
+          {
+            field: "Error",
+            message: "La persona no existe",
+          },
+        ],
+      };
     }
-    if (
-      typeof nombre !== "undefined" &&
-      nombre !== "" &&
-      nombre !== null &&
-      primer_apellido !== "" &&
-      primer_apellido !== null &&
-      dni !== "" &&
-      dni !== null &&
-      numero !== null &&
-      email !== "" &&
-      email !== null
-    ) {
-      estudiante[0].nombre = nombre;
-      estudiante[0].primer_apellido = primer_apellido;
-      estudiante[0].segundo_apellido = segundo_apellido;
-      estudiante[0].dni = dni;
-      estudiante[0].numero = numero;
-      estudiante[0].email = email;
 
-      await conn.manager.update(Persona, { id_persona }, estudiante[0]);
+    try {
+      const response = await conn
+        .createQueryBuilder()
+        .update(Persona)
+        .set(input)
+        .where("id_persona = " + input.id_persona)
+        .returning("*")
+        .execute();
+
+      return { persona: response.raw[0] };
+    } catch (error) {
+      if (error.detail.includes("already exists")) {
+        if (error.detail.includes("email")) {
+          return {
+            errors: [
+              {
+                field: "email",
+                message: "El email ya está en uso",
+              },
+            ],
+          };
+        } else if (error.detail.includes("dni")) {
+          return {
+            errors: [
+              {
+                field: "dni",
+                message: "El dni ya está en uso",
+              },
+            ],
+          };
+        }
+      }
+      return {
+        errors: [
+          {
+            field: "Error",
+            message: "Error al actualizar el estudiante",
+          },
+        ],
+      };
     }
-    return estudiante;
   }
 
   @Mutation(() => Boolean)

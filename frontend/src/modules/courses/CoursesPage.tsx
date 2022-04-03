eab6e8693027074a7109a96779413396f7496c43
Mauricio Cartagena <mc973220@gmail.com>
2022-04-03 15:21:22 -0400

    Added coursesPage

---
+++ frontend/src/modules/courses/CoursesPage.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+interface CoursesPageProps {}
+
+export const CoursesPage: React.FC<CoursesPageProps> = () => {
+  return <div>Hello World This is page is courses</div>;
+};
---
+++ frontend/src/pages/courses/index.tsx
@@ -0,0 +1,3 @@
+import { CoursesPage } from "../../modules/courses/CoursesPage";
+
+export default CoursesPage;
